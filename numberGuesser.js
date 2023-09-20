c5750fff77ca9cd808e502d8202cd97bd262a480
Teller501 <ande777j@stud.kea.dk>
2023-09-20 17:53:01 +0200

    refactored numberGuesser.js

--- numberGuesser.js
+++ numberGuesser.js
@@ -1,42 +1,61 @@
-let randomNumber = 0;
-
-function generateNumber(){
-    randomNumber = Math.floor(Math.random() * 20)+1;
-}
-
-generateNumber()
-
-function setUpHandlers() {
+const game = {
+    randomNumber: 0,
+    score: 20,
+  };
+  
+  function generateRandomNumber() {
+    game.randomNumber = Math.floor(Math.random() * 20) + 1;
+  }
+  
+  function updateScore(newScore) {
+    document.getElementById("score").textContent = newScore;
+  }
+  
+  function showMessage(message) {
+    document.getElementById("message").textContent = message;
+  }
+  
+  function check() {
+    const inputNumber = parseInt(document.getElementById("inputNumber").value, 10);
+  
+    if (isNaN(inputNumber)) {
+      showMessage("Please enter a valid number.");
+      return;
+    }
+  
+    game.score -= 1;
+    updateScore(game.score);
+  
+    if (inputNumber === game.randomNumber) {
+      showMessage("Correct!");
+  
+      if (game.score > parseInt(document.getElementById("highScore").textContent, 10)) {
+        document.getElementById("highScore").textContent = game.score;
+      }
+    } else {
+      showMessage("Wrong!");
+  
+      if (game.score <= 0) {
+        again();
+      }
+    }
+  }
+  
+  function again() {
+    generateRandomNumber();
+    game.score = 20;
+    updateScore(game.score);
+    document.getElementById("inputNumber").value = "";
+    showMessage("Start guessing...");
+  }
+  
+  function setUpHandlers() {
     document.getElementById("btnCheck").onclick = check;
     document.getElementById("btnAgain").onclick = again;
   }
-
-  setUpHandlers()
-
-  function check(){
-    let inputNumber = document.getElementById("inputNumber").value
-    let highscore = document.getElementById("highScore");
-    let score = document.getElementById("score");
-    score.innerHTML -= 1;
-    console.log(inputNumber)
-    console.log(randomNumber)
-
-    if(inputNumber == randomNumber){
-        document.getElementById("message").innerHTML = "Correct!"
-        if (highscore.innerHTML < score.innerHTML){
-            highscore.innerHTML = score.innerHTML;
-        }
-    }else {
-        document.getElementById("message").innerHTML = "Wrong!"
-        if(score.innerHTML <= 0){
-            again();
-        }
-    }
-  }
-
-  function again(){
-    generateNumber();
-    document.getElementById("score").innerHTML = 20;
-    document.getElementById("inputNumber").value = ""
-    document.getElementById("message").innerHTML = "Start guessing..."
-  }
+  
+  setUpHandlers();
+  generateRandomNumber();
+  updateScore(game.score);
+  showMessage("Start guessing...");
+  
